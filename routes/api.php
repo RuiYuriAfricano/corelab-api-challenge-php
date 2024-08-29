3acc7497d4ebc1af2397f9b4ae5728d5b4e50835
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-08-29 11:09:00 +0100

    chore: add restore route

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use App\Http\Controllers\Api\TodoController;
 
 Route::apiResource('todos', TodoController::class);
 
+// Rota para restaurar um TODO que foi deletado
+Route::patch('todos/{id}/restore', [TodoController::class, 'restore'])->name('todos.restore');
