3966435166f6d121e404be303692f3cc6390890f
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-08-29 11:07:47 +0100

    chore: add getAllTodos, getTodoById, updateTodo, deleteTodo, restoreTodo, forceDeleteTodo in service

--- app/Services/TodoService.php
+++ app/Services/TodoService.php
@@ -24,5 +24,38 @@ class TodoService
         ]);
     }
 
-    // Outros métodos de serviço podem ser adicionados aqui
+    public function getAllTodos()
+    {
+        return $this->todoRepository->getAll();
+    }
+
+    public function getTodoById($id)
+    {
+        return $this->todoRepository->find($id);
+    }
+
+    public function updateTodo($id, TodoDTO $todoDTO)
+    {
+        return $this->todoRepository->update($id, [
+            'title' => $todoDTO->title,
+            'content' => $todoDTO->content,
+            'isFavorite' => $todoDTO->isFavorite,
+            'color' => $todoDTO->color,
+        ]);
+    }
+
+    public function deleteTodo($id)
+    {
+        $this->todoRepository->delete($id);
+    }
+
+    public function restoreTodo($id)
+    {
+        return $this->todoRepository->restore($id);
+    }
+
+    public function forceDeleteTodo($id)
+    {
+        $this->todoRepository->forceDelete($id);
+    }
 }
